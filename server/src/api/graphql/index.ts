ec68bcb1644e012bf89796a54e7d70d9ebcd32a8
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-10-30 12:07:14 +0100

    2. Add GraphiQL

--- server/src/api/graphql/index.ts
+++ server/src/api/graphql/index.ts
@@ -1,7 +1,11 @@
-import { graphqlExpress } from 'graphql-server-express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 
 export default function (app) {
   app.use('/graphql', graphqlExpress({
     // options
+  }));
+
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
   }));
 }
